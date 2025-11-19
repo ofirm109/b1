c1ab223d857a8a8938d1536c0c39230fd69796d9
ofir <ofirm109@walla.com>
2025-11-19 15:45:01 +0200

    1545

---
+++ story_content/user.js
@@ -0,0 +1,285 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5mFcqHCmK8E":
+        Script1();
+        break;
+      case "5ViV6Cmo9Q5":
+        Script2();
+        break;
+      case "6d8T7xQ70Jx":
+        Script3();
+        break;
+      case "5aLalRxM5ZS":
+        Script4();
+        break;
+      case "61D4O4eSAuo":
+        Script5();
+        break;
+      case "5bmifTK4DMM":
+        Script6();
+        break;
+      case "6r7KfZrsaUv":
+        Script7();
+        break;
+      case "5flcNCwNOdK":
+        Script8();
+        break;
+      case "6Ag9WCgDMNy":
+        Script9();
+        break;
+      case "6Dl6nbNvpnn":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  var myParam = location.search.split('p=')[1];
+var myParam1 =decodeURIComponent(myParam);
+
+var myParam2 = myParam1.split('|')[0];
+var myParam3 = myParam1.split('|')[1];
+
+var player = GetPlayer();
+
+player.SetVar("p1",myParam2);  
+
+player.SetVar("p2",myParam3);  
+
+}
+
+function Script2()
+{
+  var myParam = location.search.split('p=')[1];
+var myParam1 =decodeURIComponent(myParam);
+
+var myParam2 = myParam1.split('|')[0];
+var myParam3 = myParam1.split('|')[1];
+
+var player = GetPlayer();
+
+player.SetVar("p1",myParam2);  
+
+player.SetVar("p2",myParam3);  
+
+}
+
+function Script3()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script4()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script5()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script6()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script7()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script8()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script9()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+
+function Script10()
+{
+  if( window.cssPatchWebObject !== "done") {
+
+  style = document.createElement('style');
+
+  style.textContent = `
+
+    .webobject {
+
+      width:  100% !important;
+
+      height: 100% !important;
+
+      transform: none !important;
+
+     };`
+
+ 
+
+  document.body.appendChild(style);
+
+  window.cssPatchWebObject = "done";
+
+}
+}
+

1c00be1a8c8efa26915b51bac71691c76386be4e
ofir <ofirm109@walla.com>
2026-05-05 17:18:12 +0300

    1718

---
+++ story_content/triggers.js
@@ -0,0 +1,32 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ke2wagO6t5":
+        Script1();
+        break;
+      case "60X6fK37QUb":
+        Script2();
+        break;
+      case "5bs3CaSdIwF":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
